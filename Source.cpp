bebb13bac3bf6f8665c51c465ba4d41025e417dc
BmsOS-Dev <152536945+BmsOS-Dev@users.noreply.github.com>
2023-12-06 15:01:01 -0500

    Create Source.cpp
    And include base code for program init

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+     if (argc < 2) {
+        printf("X arguments are necessary.");\
+        printf("\n<>");
+        return 1;
+    }
+    
+    printf("Program name : IPV4 Pinging tool");
+}
